105209fb0f1cc64913ec803015e414a4eb353bcf
AmazinglyDifferent <90712938+AmazinglyDifferent@users.noreply.github.com>
2022-03-02 03:58:13 +0530

    Update Lab_9_Prob_1.c
    ASCII Value of Your Name

--- LabWork/CSBLab9/Lab_9_Prob_1.c
+++ LabWork/CSBLab9/Lab_9_Prob_1.c
@@ -5,8 +5,10 @@ int main(){
     char name[30];
     printf("Enter your name:");
     gets(name);
+    
     for(int i=0; i<strlen(name); i++){
         printf("ASCII Value of %d character is: %d\n",i+1,name[i]);
     }
+    
     return 0;
 }
