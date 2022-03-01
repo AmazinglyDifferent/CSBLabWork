4a2a928bf3337aefd03cd5560dc9039666ee1c26
AmazinglyDifferent <90712938+AmazinglyDifferent@users.noreply.github.com>
2022-03-02 03:56:44 +0530

    Update Lab_9_Prob_1.c

--- LabWork/CSBLab9/Lab_9_Prob_1.c
+++ LabWork/CSBLab9/Lab_9_Prob_1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+
 int main(){
     char name[30];
     printf("Enter your name:");
-    scanf("%s",&name);
+    gets(name);
     for(int i=0; i<strlen(name); i++){
         printf("ASCII Value of %d character is: %d\n",i+1,name[i]);
     }
