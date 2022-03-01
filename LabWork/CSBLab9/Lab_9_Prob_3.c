f0044d7ae707637e01baf28cb92a0414cbffa6ca
AmazinglyDifferent <90712938+AmazinglyDifferent@users.noreply.github.com>
2022-03-02 04:29:57 +0530

    Counting Characters, Words and Lines in a file

--- LabWork/CSBLab9/Lab_9_Prob_3.c
+++ LabWork/CSBLab9/Lab_9_Prob_3.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+
 int main(){
+    
     char string[30];
     int charcount= 0;
     int wordcount = 0;
     int linecount = 0;
+
     printf("Enter the string:");
     scanf("%[^*]s", string);
     printf("You entered: %s\n",string);
-    for(int i = 0; i < strlen(string); i++) {  
-        if(string[i] != ' ' || string[i] != '\n' )  
-            charcount++;  
-    }
-    for(int i = 0; i < strlen(string); i++) {  
-        if(string[i] == ' ' || string[i] == '\n')  
-            wordcount++;  
-    }
-    for(int i = 0; i < strlen(string); i++) {  
-        if(string[i] == '\n')  
-            linecount++;  
-    } 
 
+    char *p = string;
+    while(*p != '\0'){
+        if(*p != ' ' && *p != '\n' ){  
+            charcount++;  
+        }
+        if(*p == ' ' || *p == '\n'){ 
+            wordcount++;  
+        }
+        if(*p == '\n'){
+            linecount++;  
+        }
+        p++;
+    }
+    
     printf("Number of Characters: %d\n",charcount);
     printf("Number of Words: %d\n",wordcount);
     printf("Number of Lines: %d\n",linecount);
+
     return 0;
 }
