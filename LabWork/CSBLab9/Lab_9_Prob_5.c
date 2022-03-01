0c74c5d0a35e3dd719baebffd6fccba7e9f02eb6
AmazinglyDifferent <90712938+AmazinglyDifferent@users.noreply.github.com>
2022-03-02 04:42:19 +0530

    Sum of elements of array using pointers

--- LabWork/CSBLab9/Lab_9_Prob_5.c
+++ LabWork/CSBLab9/Lab_9_Prob_5.c
@@ -6,13 +6,16 @@ int main(){
         printf("Enter %d element:",i+1);
         scanf("%d",&array[i]);
     }
+    
     int *p;
     p=array;
     int sum=0;
-    while(*p){
+    
+    while(*p!='\0'){
         sum= sum + *p;
         p++;
     }
+    
     printf("Sum of all elements are: %d\n",sum);
     return 0;
 }
