38dac491cf404eef234db9952b3872c455b31021
AmazinglyDifferent <90712938+AmazinglyDifferent@users.noreply.github.com>
2022-03-03 09:19:41 +0530

    Linear Search using int pointers

--- LabWork/CSBLab9/Lab_9_Prob_8_1.c
+++ LabWork/CSBLab9/Lab_9_Prob_8_1.c
@@ -1,28 +1,38 @@
 #include<stdio.h>
-  
-int main()
-{
-    int array[20],i,search,num;
-    printf("Enter number of elements : ");
-    scanf("%d",&num);
-     
-    printf("Enter array elements :");
-    for(i=0;i<num;++i)
-        scanf("%d",&array[i]);
-     
-    printf("Enter element to search:");
-    scanf("%d",&search);
-     
+
+int linearsearch(int array[], int search, int num){
+    int *p = array;
+    int i;
     for(i=0;i<num;++i){
-        if(array[i]==search)
+        if(*p==search)
             break;
+        p++;
     } 
     if(i<num){
         printf("Element found at index %d",i);
+        return i;
     }
     else{
-        printf("Element not found");
+        return -1;
     }
-  
+}
+
+int main()
+{   
+    int num;
+    printf("Enter number of elements :\n");
+    scanf("%d",&num);
+
+    int array[20],search;
+    
+    printf("Enter array elements :\n");
+    for(int i=0;i<num;++i)
+        scanf("%d",&array[i]);
+     
+    printf("Enter element to search:\n");
+    scanf("%d",&search);
+    
+    linearsearch(array,search,num); 
+    
     return 0;
 }
