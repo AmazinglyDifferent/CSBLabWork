7518bcb3e39f9f2dbac3ae6bf51dc06fef299242
AmazinglyDifferent <90712938+AmazinglyDifferent@users.noreply.github.com>
2022-03-13 20:44:59 +0530

    Update Lab_10_Prob_5.c

--- LabWork/CSB Lab10/Lab_10_Prob_5.c
+++ LabWork/CSB Lab10/Lab_10_Prob_5.c
@@ -1,46 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Account {
-    long int Accountnum;
+struct Account{
+    long Accountnum;
     char Name[50];
     long int balance;
 };
 
-void Accountdata(struct Account Accounts[2]){
-    for (int i=0; i<2; i++){
+void Accountdata(struct Account *Accounts, int num){
+    for (int i=0; i<num; i++){
         scanf("%ld",&Accounts[i].Accountnum);
-        scanf("%[^\n]s",Accounts[i].Name);
+        scanf(" %[^\n]s",Accounts[i].Name);
         scanf("%ld",&Accounts[i].balance);
     }
 }
 
-void deporwithdraw(struct Account Accounts[2]){
+void deporwithdraw(struct Account *Accounts, int n){
     long int num;
+    printf("\n Enter account number : ");
     scanf("%ld",&num);
-    for(int i=0; i<2;i++){
+
+    int count = 0;
+    for(int i=0; i<n;i++){
         if(num == Accounts[i].Accountnum){
-            printf("1 for Deposit, 0 for Withdrawal\n");
+            
             int amount;
             printf("Enter amount to deposit or withdraw : ");
             scanf("%d",&amount);
+
+            printf("1 for Deposit, 0 for Withdrawal\n");
             int choice;
+
             scanf("%d",&choice);
             if(choice == 1){
                 Accounts[i].balance = Accounts[i].balance + amount;
+                printf("\n Deposite successfull. New balance : %ld",Accounts[i].balance);
+    
             }
             else if (choice == 0){
                 if(Accounts[i].balance - amount < 1000)
                     printf("The Balance is insufficient for specified withdrawal\n");
                 else
+                {
                     Accounts[i].balance = Accounts[i].balance - amount;
+                    printf("\n Withdrawal successfull. Remaining balance : %ld",Accounts[i].balance);
+                }
             }
+
+            count++;
         }
+        
+    }
+
+    if(count == 1)
+    {
+        //do nothing
+    }
+    else {
+        printf("\n Account not found.");
     }
 }
 
-void displayaccountslessthan(struct Account Accounts[2]){
-    for (int i=0; i<2; i++){
+void displayaccountslessthan(struct Account *Accounts, int num){
+    for (int i=0; i<num; i++){
         if (Accounts[i].balance < 1000){
             printf("Account Num : %ld\n", Accounts[i].Accountnum);
             printf("Account Name : %s\n", Accounts[i].Name);
@@ -50,16 +72,17 @@ void displayaccountslessthan(struct Account Accounts[2]){
 
 int main(){
 
-    struct Account Accounts[2];
+    int input = 2;
+    struct Account Accounts[input];
 
-    Accountdata(Accounts);
-    displayaccountslessthan(Accounts);
+    Accountdata(Accounts, input);
+    displayaccountslessthan(Accounts, input);
 
-    char input;
-    printf("Want to deposit/withdraw?:  Y/N");
-    scanf("%c",&input);
-    if (input == 'Y' || input == 'y'){
-        deporwithdraw(Accounts);
+    char c;
+    printf("\n Want to deposit/withdraw?:  Y/N");
+    scanf(" %c",&c);
+    if (c == 'Y' || c == 'y'){
+        deporwithdraw(Accounts, input);
     }
     else 
         printf("Okay\n");
